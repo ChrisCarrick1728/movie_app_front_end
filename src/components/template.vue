21483ef3dcf54c3da00ee317e1a01a6866cdcb77
Chris Carrick-2 <car17059@byui.edu>
2019-02-12 21:11:49 -0700

    commit

---
+++ src/components/template.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style>
+h1, a {
+  color: white;
+}
+
+</style>
